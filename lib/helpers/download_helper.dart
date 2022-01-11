02806825addb1b0de5770889aa731d2fc5537fa9
mfaizk <faizk123485@gmail.com>
2022-01-11 14:04:46 +0530

    linux function are all completed, fish shell is tested zsh and bash need to tested

--- lib/helpers/download_helper.dart
+++ lib/helpers/download_helper.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:io';
-
+import 'package:path/path.dart' as p;
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:path_provider/path_provider.dart';
@@ -50,6 +50,105 @@ class DownloadHelper extends ChangeNotifier {
       notifyListeners();
     });
     // print(response.headers);
+    // getHomepath();
+  }
+
+  unArchiver(String location, path) async {
+    if (Platform.isLinux) {
+      await Directory(path + "/.envSdk").create();
+      String locToExtractFolder = path + "/.envSdk";
+      String gitInitPah = locToExtractFolder + "/flutter-master/";
+
+      if (!(await Directory(gitInitPah).exists())) {
+        Shell().run('''
+    
+    unzip $location -d $locToExtractFolder 
+    git init $gitInitPah
+    ''').whenComplete(() {
+          status = "Extraction Completed";
+          progress = 1.0;
+          setPathInLinux();
+          notifyListeners();
+        }).catchError((e) {
+          status = e.toString();
+          notifyListeners();
+        });
+      } else {
+        Directory(gitInitPah).delete(recursive: true).whenComplete(() {
+          Shell().run('''
+    
+    unzip $location -d $locToExtractFolder 
+    git init $gitInitPah
+    ''').whenComplete(() {
+            status = "Extraction Completed";
+            progress = 1.0;
+            setPathInLinux();
+            notifyListeners();
+          }).catchError((e) {
+            status = e.toString();
+            notifyListeners();
+          });
+        });
+      }
+    }
+  }
+
+  Future shellChecker() async {
+    String? currentShell;
+    var shellC = shellEnvironment.entries;
+    for (var item in shellC) {
+      if (item.key == "SHELL") {
+        currentShell = item.value;
+        print(currentShell);
+      }
+    }
+    return currentShell;
+  }
+
+  //linux specificCode
+  void setPathInLinux() async {
+    // print('yo');
+
+    String currentShell = await shellChecker();
+    status = 'Setting Path';
+    notifyListeners();
+    try {
+      if (currentShell.toString().contains('fish')) {
+        File(p.joinAll([getHomepath(), '.config', 'fish', 'config.fish']))
+            .writeAsStringSync(
+                "\nset fish_user_paths " +
+                    p.join(await getPath(), '.envSdk', 'flutter-master',
+                        'bin' + (" :\$fish_user_paths\n")),
+                mode: FileMode.append);
+      } else if (currentShell.toString().contains('zsh')) {
+        // print('zsh');
+        File(p.joinAll([
+          getHomepath(),
+          '.zshrc',
+        ])).writeAsStringSync(
+            "\nPATH=" +
+                p.join(
+                    path, '.envSdk', 'flutter-master', 'bin' + (" :\$PATH\n")),
+            mode: FileMode.append);
+      } else if (currentShell.toString().contains('bash')) {
+        File(p.joinAll([getHomepath(), '.bashrc'])).writeAsStringSync(
+            "\nPATH=" +
+                p.join(
+                    path, '.envSdk', 'flutter-master', 'bin' + (" :\$PATH\n")),
+            mode: FileMode.append);
+        // print('bash');
+      } else {
+        status =
+            'Shell not recognised we only support fish,zsh and bash shell currently';
+        notifyListeners();
+      }
+    } on Exception catch (e) {
+      status = e.toString();
+      notifyListeners();
+    }
+    dStared = !dStared;
+    status = 'Installation Completed';
+    notifyListeners();
   }
 
   Future getPath() async {
@@ -60,26 +159,19 @@ class DownloadHelper extends ChangeNotifier {
     return dPath;
   }
 
-  unArchiver(String location, path) async {
-    if (Platform.isLinux) {
-      await Directory(path + "/.envSdk").create();
-      String locToExtractFolder = path + "/.envSdk";
-      String gitInitPah = locToExtractFolder + "/flutter-master/";
-      Shell().run('''
-    unzip $location -d $locToExtractFolder
-    ''').whenComplete(() {
-        status = "Extraction Completed";
-        progress = 1.0;
-        notifyListeners();
-      }).catchError((e) {
-        print(e.toString());
-      });
-
-      Shell().run('''
-  git init $gitInitPah
-  ''');
+  String getHomepath() {
+    String os = Platform.operatingSystem;
+    String home = "";
+    Map<String, String> envVars = Platform.environment;
+    if (Platform.isMacOS) {
+      home = envVars['HOME'].toString();
+    } else if (Platform.isLinux) {
+      home = envVars['HOME'].toString();
+    } else if (Platform.isWindows) {
+      home = envVars['UserProfile'].toString();
     }
+    stdout.writeln(home);
+    return home;
   }
-
   // print(response.headers);
 }
